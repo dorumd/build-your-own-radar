0a9b189ecc065c486948a30915a3c4f72d924e60
Dorel Mardari <mardari.dorua@gmail.com>
2020-08-19 22:07:21 +0200

    Use env variable for sheet id

--- src/site.js
+++ src/site.js
@@ -4,5 +4,6 @@ require('./images/radar_legend.png')
 require('./gtm.js')
 
 const GoogleSheetInput = require('./util/factory')
+const sheetId = process.env.SHEET_ID
 
-GoogleSheetInput().build_from_sheet('http://localhost:8090/artifacts/radar.csv')
+GoogleSheetInput().build_from_sheet(sheetId)
--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,8 @@ const plugins = [
     'process.env.CLIENT_ID': JSON.stringify(process.env.CLIENT_ID),
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
     'process.env.ENABLE_GOOGLE_AUTH': JSON.stringify(process.env.ENABLE_GOOGLE_AUTH),
-    'process.env.GTM_ID': JSON.stringify(process.env.GTM_ID)
+    'process.env.GTM_ID': JSON.stringify(process.env.GTM_ID),
+    'process.env.SHEET_ID': JSON.stringify(process.env.SHEET_ID),
   })
 ]
 
